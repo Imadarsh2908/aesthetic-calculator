ebf204fc75fda5a7de9a503ac1e814484852b6ff
Adarsh giri <159279916+Imadarsh2908@users.noreply.github.com>
2026-05-22 11:52:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var Buttons = document.querySelectorAll('.btn');
+    var inputBox = document.getElementById('result-box');
+
+    Buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+
+            var btnvalue = this.value;
+
+            if (btnvalue === "del") {
+                inputBox.value = inputBox.value.slice(0, -1);
+            }
+
+            else if (btnvalue === "delall") {
+                inputBox.value = inputBox.value.slice(0, 0)
+            }
+
+            else if (btnvalue === "=") {
+                inputBox.value = eval(inputBox.value);
+            }
+
+            else if (btnvalue === "%") {
+                inputBox.value = (inputBox.value/100) + "*";
+            }
+
+            else {
+                inputBox.value += this.value;
+            }
+        });
+    });
+});
